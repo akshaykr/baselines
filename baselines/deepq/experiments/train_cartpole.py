3adb74b0fb82ccd69624f7c9d9c2dc0d1658106d
akshay <krish.akshay@gmail.com>
2018-10-24 13:00:10 -0400

    reverting train_cartpole changes"

--- baselines/deepq/experiments/train_cartpole.py
+++ baselines/deepq/experiments/train_cartpole.py
@@ -1,15 +1,7 @@
-import sys
-sys.path.append('/Users/akshay/code/baselines/')
-sys.path.append('/Users/akshay/projects/state_decoding/exp_contextual/python/')
 import gym
-from gym.envs.registration import register
 
 from baselines import deepq
 
-register(
-    id = 'Grid-v0',
-    entry_point='Environments:GridWorld',
-)
 
 def callback(lcl, _glb):
     # stop training if reward exceeds 199
@@ -18,21 +10,20 @@ def callback(lcl, _glb):
 
 
 def main():
-    env = gym.make("Grid-v0")
-    env.init(env_config={'horizon':2, 'dimension':2, 'swap':0.1})
-
+    env = gym.make("CartPole-v0")
     act = deepq.learn(
         env,
         network='mlp',
         lr=1e-3,
-        total_timesteps=10000*env.horizon,
+        total_timesteps=100000,
         buffer_size=50000,
         exploration_fraction=0.1,
         exploration_final_eps=0.02,
-        prioritized_replay=True,
-        print_freq=100,
-        callback=None
+        print_freq=10,
+        callback=callback
     )
+    print("Saving model to cartpole_model.pkl")
+    act.save("cartpole_model.pkl")
 
 
 if __name__ == '__main__':
